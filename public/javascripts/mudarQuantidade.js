81a5fcedf7f8d0751df05132366821dcf41b3b57
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2022-03-14 20:25:22 -0300

    tentativa de concertar a quantidade do carrinho

--- public/javascripts/mudarQuantidade.js
+++ public/javascripts/mudarQuantidade.js
@@ -1,11 +1,11 @@
 function decrementa(){
-    let quantidade = document.getElementById("quantidadeItens").innerHTML.value
+    let quantidade = document.getElementById("quantidadeItens").value
     quantidade = quantidade -1
-    document.getElementById("quantidadeItens").innerHTML = quantidade
+    document.getElementById("quantidadeItens").value = quantidade
 }
 
 function incrementa(){
-    let quantidade = document.getElementById("quantidadeItens").innerHTML.value
+    let quantidade = document.getElementById("quantidadeItens").value
     quantidade = quantidade +1
-    document.getElementById("quantidadeItens").innerHTML = quantidade
+    document.getElementById("quantidadeItens").innerHTML.value = quantidade
 }
