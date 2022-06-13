4ce90fff5b50c2fdd076f0dbae3fbef6a7ea1155
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2022-06-12 21:58:35 -0300

    feat: adding the Listings component

--- square-react/src/views/Home.jsx
+++ square-react/src/views/Home.jsx
@@ -1,11 +1,13 @@
 import HeroBlocks from "../components/Home/HeroBlocks"
 import Features from "../components/Home/Features"
+import Listings from "../components/Home/Listings"
 
 function Home(){
     return(
         <section>
             <HeroBlocks/>
             <Features/>
+            <Listings/>
         </section>
     )
 }
